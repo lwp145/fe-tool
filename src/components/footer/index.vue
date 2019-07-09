e920175d9a96be7194c0ff373ce272d87d6989ce
chenzejiang <820289461@qq.com>
2019-07-09 11:23:49 +0800

    重命名

---
+++ src/components/footer/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="footer">
+    Copyright © 2017 - {{this.year}}
+    <a href="http://www.chenzejiang.com" title="陈泽江的博客">webJ</a> All Rights Reserved
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.footer {
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  background: #373737;
+  border-bottom: 2px solid #434343;
+  color: #848484;
+  font-size: 14px;
+  text-align: center;
+  a {
+    text-decoration: none;
+    color: #ababab;
+  }
+}
+</style>
